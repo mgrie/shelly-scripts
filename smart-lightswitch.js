52b836146ff9034b7a209d4871ad6e5e94284db6
Marco Grießhammer <griesshammer@yamud.de>
2024-05-21 22:33:21 +0200

    Use Shelly auto_off_delay instead of Timer
    Improvements:
    
     - Timer Animation in Shelly App
     - Better compatibility with 'Out of the Box Standard'
     - less code

--- smart-lightswitch.js
+++ smart-lightswitch.js
@@ -1,91 +1,82 @@
 print("Start SmartLightSwitch Script");
 
 let CONFIG = {
-  time1: 5, // 2 Minuten
+  time1: 2*60, // 2 Minuten
   time2: 5*60, // 5 Minuten
   time3: 10*60, // 10 Minuten
   timelong: null // Dauerlicht
 };
 
-let timerHandle = null;
-
-function buttonEvent(time){
-  print("Start Timer: " + time);
-    
-  if(!Shelly.getComponentStatus("switch:0").output){
-    switchLightOn(time);
+function toogleSwitch(delay, callback){
+  if(isSwitchOn()){
+    setSwitchOff(callback);
   } else {
-    switchLightOff();
+	setSwitchOn(delay, callback);
   }  
 }
 
-function stopTimer(){
-  if(timerHandle !== null){
-    print("Stop Timer");
-    Timer.clear(timerHandle);
-    timerHandle = null;
-  }
+function isSwitchOn(){
+	return Shelly.getComponentStatus("switch:0").output;
 }
 
-function switchLightOn(time){
-  print("switchLight On");
-  
-  Shelly.call("Switch.set", {'id': 0, 'on': true});
- 
-  if(time !== null){
-    timerHandle = Timer.set(
-      time*1000, 
-      false, 
-      function(ud){
-        print("End Timer");
-        switchLightOff();
-      },
-      null
-    );  
-  } else {
-    print("Dauerlicht!");
-  }
-
+function setAutoOffFalse(callback){
+  // print('Set AutoOff false');
+  Shelly.call("Switch.SetConfig", {'id': 0, 'config': {'auto_off': false}}, callback);
 }
 
-function switchLightOff(){
-    print("switchLight Off");
-    // stopTimer();
-    Shelly.call("Switch.set", {'id': 0, 'on': false});  
+function setAutoOffDelay(delay, callback){
+	if(delay > 0){
+	    // print('Set AutoOff Delay: ' + delay);
+		Shelly.call("Switch.SetConfig", {'id': 0, 'config': {'auto_off': true, 'auto_off_delay': delay}}, callback);
+	} else {
+		setAutoOffFalse(callback);
+	}
+}
+
+function setSwitchOn(delay, callback){
+    // print("Set Switch On, Delay: " + delay);
+	setAutoOffDelay(delay, function(ud){
+		Shelly.call("Switch.set", {'id': 0, 'on': true}, callback);
+	});
+}
+
+function setSwitchOff(callback){
+    // print("Set Switch Off");
+	Shelly.call("Switch.set", {'id': 0, 'on': false}, callback);
 }
 
 Shelly.addEventHandler(function(e) {
   
   // Input Button
   if (e.component === "input:0") {
-    if (e.info.event === "single_push") {
-      print("Button was pushed");
-      buttonEvent(CONFIG.time1);
-    }
-    
-    if (e.info.event === "double_push") {
-      print("Button was double pushed");
-      buttonEvent(CONFIG.time2);
-    }
-    
-    if (e.info.event === "triple_push") {
-      print("Button was triple pushed");
-      buttonEvent(CONFIG.time3);
-    }
-    
-    if (e.info.event === "long_push") {
-      print("Button was long pushed");
-      buttonEvent(CONFIG.timelong)
+    switch (e.info.event) {
+      case "single_push":
+        // print("Button was pushed");
+        toogleSwitch(CONFIG.time1);
+        break;
+      case "double_push":
+        //print("Button was double pushed");
+        toogleSwitch(CONFIG.time2);
+        break;
+      case "triple_push":
+        //print("Button was triple pushed");
+        toogleSwitch(CONFIG.time3);
+        break;
+      case "long_push":
+        //print("Button was long pushed");
+        toogleSwitch(CONFIG.timelong);
+        break;
     }
   }
   
   // Output Light Switch
   if (e.component === "switch:0") {
-    if(!e.info.state && timerHandle !== null){
-      stopTimer();
-    }
+    // could be undefined!
+    if(e.info.state === false){
+      // print('Switch Off Dedection');
+      setAutoOffFalse();
+    }  
   }
-  
 });
 
 print("Script is running");
