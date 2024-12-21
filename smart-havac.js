55541877f753db669258663ec5278723f9a3994e
Marco Grießhammer <griesshammer@yamud.de>
2024-12-22 00:54:22 +0100

    WIP

--- smart-havac.js
+++ smart-havac.js
@@ -1,66 +1,108 @@
-let currentTempHandler = Virtual.getHandle('number:200');
-let targetTempHandler = Virtual.getHandle('number:201');
-let hysteresis = 0.2;
+/// <reference path="../../shelly-script.d.ts" />
 
-function checkHeating(targetTemp, currentTemp, hysteresis, isHeating){
-  
-  if(isHeating){
-    if(currentTemp - targetTemp < 0){
-      print('weiter heizen');
-      return true;
-    } else {
-      print('nicht mehr heizen');
-      return false;
-    }
+print("Bootstrap");
+
+let CONFIG = {
+  mqttTopicPrefix: 'shellypro4pm-fbh',
+  mqttClientId: 'shellypro4pm-fbh'
+};
+
+let ENTITIES = [
+  {
+    valveId: 0,
+    hysteresis: 0.2,
+    currentTempId: 'number:200',
+    targetTempId: 'number:201'
   }
-  else {
-    if(targetTemp - hysteresis >= currentTemp){
-      print('starte heizen');
-      return true;
-    } else {
-      print('warm genug, nicht heizen');
-      return false;
-    }
-  } 
+]
+
+
+function isHeatingRequired(targetTemp, currentTemp, hysteresis, isHeating) {
+  // If we're currently heating, continue heating until we reach the target temperature
+  if (isHeating) {
+    return currentTemp < targetTemp;
+  }
+
+  // If we're not heating, start heating if the current temperature is below the lower threshold
+  return currentTemp <= targetTemp - hysteresis;
 }
 
-function havacLoop(){
-  let targetTemp = targetTempHandler.getValue();
-  let currentTemp = currentTempHandler.getValue();
-  let currentValveState = Shelly.getComponentStatus('switch:0').output;
+function havacLoop(entity){
+
+  let targetTemp = entity.targetTempHandler.getValue();
+  let currentTemp = entity.currentTempHandler.getValue();
+  let currentValveState = Shelly.getComponentStatus('switch:' + entity.valveId).output;
   
-  let nextValveState = checkHeating(targetTemp, currentTemp, hysteresis, currentValveState );
+  let nextValveState = isHeatingRequired(targetTemp, currentTemp, entity.hysteresis, currentValveState );
   if(currentValveState !== nextValveState ) {
      print('Neuer Ventilstatus: ' + nextValveState);
-     Shelly.call("Switch.set", {'id': 0, 'on': nextValveState});
+     Shelly.call("Switch.set", {'id': entity.valveId, 'on': nextValveState});
   }   
   
-  sendStatusMessage(targetTemp, currentTemp, nextValveState);
+  sendStatusMessage(entity.valveId, targetTemp, currentTemp, nextValveState);
 }
 
-function sendStatusMessage(targetTemp, currentTemp, valveState) {
+
+
+function sendStatusMessage(valveId, targetTemp, currentTemp, valveState) {
   var message = {
     target_T: targetTemp,
     current_T: currentTemp,
     action: valveState ? 'heating' : 'off',
-    valveId: 0
+    clientId: CONFIG.mqttClientId,
+    valveId: valveId
   };
   
-  MQTT.publish('shellypro4pm-fbh/havacstatus0', JSON.stringify(message) );
+  MQTT.publish(CONFIG.mqttTopicPrefix +  '/havac/status/'  + valveId, JSON.stringify(message) );
 }
 
-Shelly.addStatusHandler(function(e, ud){
-  
-  if(!e) return;
-  if(e.component === 'number:200' || e.component === 'number:201'){
-    havacLoop();      
+
+function initEntities(entities) {
+  for (const entity of entities) {
+    entity.currentTempHandler = Virtual.getHandle(entity.currentTempId);
+    entity.targetTempHandler = Virtual.getHandle(entity.targetTempId);
   }
-  
-});
 
-MQTT.subscribe('shellypro4pm-fbh/havacstatus0/setT', function(topic, message){
-  //var data = JSON.parse(message);
-  targetTempHandler.setValue(message);
-});
+  return entities;
+}
 
-havacLoop();
+
+function registerStatusHandler(entites){
+  const componentMap = {};
+
+  for (const entity of entites) {
+    componentMap[entity.currentTempId] = entity;
+    componentMap[entity.targetTempId] = entity;
+  }
+
+  Shelly.addStatusHandler(function(e, map){
+    if(!e) return;
+
+    let entity = map[e.component];
+    if(entity){
+      havacLoop(entity);
+    }
+  }, componentMap);
+};
+
+
+/*
+function subscribeMqtt(entites){
+  MQTT.subscribe(CONFIG.mqttTopicPrefix + 'havac/set/#', function(topic, message){
+    //var data = JSON.parse(message);
+    targetTempHandler.setValue(message);
+  });
+};
+*/
+
+function main() {
+  // load Entities
+  let entities = initEntities(ENTITIES);
+  registerStatusHandler(entities);
+  // subscribeMqtt(entities);
+
+  print('Anwendung gestartet');
+
+}
+
+main();
