e62d5e2d7b4c8c9b8e006a00d003f526fad0c79c
Marco Grießhammer <griesshammer@yamud.de>
2024-12-31 17:56:47 +0100

    Update log-restart.js

--- log-restart.js
+++ log-restart.js
@@ -1,6 +1,56 @@
 /// <reference path="../../shelly-script.d.ts" />
 
-Timer.set(20000, false, function(){
-  MQTT.publish('shellyblugwg3-lehrerzimmer/log', 'Restart detected!');
-})
+let DYNCONFIG = {
+  mqttTopicPrefix: undefined,
+  mqttClientId: undefined
+};
 
+function log(message){
+  try {
+      if(typeof message === 'object' ){
+          message = JSON.stringify(message);
+      } else if (typeof message !== 'string'){
+          message = message.toString();
+      }
+   
+      print(message);
+
+      // Shelly.emitEvent("log", message);
+      
+      if(MQTT.isConnected()) {
+          MQTT.publish(DYNCONFIG.mqttTopicPrefix + "/log", message);
+      }
+  } catch (error) {
+      print("Error: " + JSON.stringify(error));
+  }
+}
+
+function waitForMqtt(callback){
+  if(MQTT.isConnected){
+    callback();
+  } else {
+    let timerHandle = Timer.set(2000, true, function(){
+      if(MQTT.isConnected){
+        Timer.clear(timerHandle);
+        callback();
+      }
+    });
+  }
+}
+
+function init(callback){
+  Shelly.call('MQTT.GetConfig', {}, function(result, error_code, error_message, userdata){
+    if(error_code === 0 && result){
+      DYNCONFIG.mqttTopicPrefix = result.topic_prefix;
+      DYNCONFIG.mqttClientId = result.client_id;
+    }
+    waitForMqtt(callback);
+  });
+}
+
+function main(){
+    // ToDo: Get restart Timestamp etc.
+    log('Restart detected!');  
+}
+
+init(main);
