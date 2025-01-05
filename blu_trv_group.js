b84270246dc1bd5338f9cf37ce481310dbeaf5c6
Marco Grießhammer <griesshammer@yamud.de>
2025-01-05 11:45:07 +0100

    Update blu_trv_group.js

--- blu_trv_group.js
+++ blu_trv_group.js
@@ -5,15 +5,20 @@
  *
  * Autor:   Marco Grießhammer (https://github.com/mgrie)
  * Date:    2025-01-05
- * Version: 0.4 alpha
+ * Version: 0.4
  * Github:  https://github.com/mgrie/shelly-scripts/blob/main/smart-lightswitch.js
  *
+ * ToDo: autoconfig not implemented.
+ * ToDo: prevent second target temperature trigger
+ * 
+ * Disable all Slaves to support 'SetPosition' feature by calling 'Trv.SetConfig', example for TRV Id 202:
+ * http://192.168.xxx.xxx/rpc/BluTrv.Call?id=202&method=TRV.SetConfig&params={id:0,config:{enable:false}}
  * 
  * WARNING: Not tested with more than 1 Slaves. The Trv.SetPosition calls could be to fast for the device.
  */
 
 const CONFIG = {
-  autoconfig: true,
+  autoconfig: false,
   master: {
       trvId: 201,
       targetTempSensor: 'bthomesensor:225'
@@ -57,7 +62,7 @@ function log(message){
 // init script environment
 function initEnv(callback){
   Shelly.call('MQTT.GetConfig', {}, function(result, error_code, error_message, userdata){
-    if(error_code) log({error_code, error_message});
+    if(error_code) log({error_code: error_code, error_message: error_message});
     
     if(error_code === 0 && result){
       ENV.mqttTopicPrefix = result.topic_prefix;
@@ -87,7 +92,7 @@ function setNewTargetTemparature(sourceId, targetMap, target_C){
         log("Set new target temperature " + target_C + " for TRV: " + targetMap[key]); 
 
         Shelly.call("BluTrv.call", { id: targetMap[key], method: 'Trv.SetTarget', params: {id:0, target_C: target_C}}, function(result, error_code, error_message){
-          if(error_code) log({error_code, error_message});
+          if(error_code) log({error_code: error_code, error_message: error_message});
           if(result) log(result);
         });
       }
@@ -95,21 +100,25 @@ function setNewTargetTemparature(sourceId, targetMap, target_C){
   }
 }
 
-function syncSlaves(slaves, newValvePosition){
-  for(slave in slaves){
+function syncSlavesValvePosition(slaves, newValvePosition){
+  slaves.forEach(function(slave){
+    setValvePosition(slave, newValvePosition);
+  });
+}
 
-    if(newValvePosition != slave.lastValvePosition){
-      log('Set valve position of ' + slave.trvId + ' to: ' + newValvePosition);
-      Shelly.call('BluTrv.Call', {id:slave.trvId, method: 'TRV.SetPosition', params:{id:0, pos: newpos}}, function(result, error_code, error_message, ud){
-        if(error_code) {
-          log({error_code, error_message});
-        } 
-        else {
-          ud.slave.lastValvePosition = ud.newValvePosition;
-        }
-      }, {slave, newValvePosition});
-    }
-  }
+function setValvePosition(targetTrv, newValvePosition){
+  // only if change detected
+  if(newValvePosition != targetTrv.lastValvePosition){
+    Shelly.call('BluTrv.Call', {id:targetTrv.trvId, method: 'TRV.SetPosition', params:{id:0, pos: newValvePosition}}, function(result, error_code, error_message, ud){
+      if(error_code) {
+        log({error_code: error_code, error_message: error_message});
+      } 
+      else {
+        log('Set valve position of ' + ud.targetTrv.trvId + ' to: ' + ud.newValvePosition);
+        ud.targetTrv.lastValvePosition = ud.newValvePosition;
+      }
+    }, {targetTrv: targetTrv, newValvePosition: newValvePosition});    
+  }  
 }
 
 function registerHandlers(){
@@ -145,10 +154,10 @@ function registerHandlers(){
       Shelly.call('BluTrv.Call', {id:config.master.trvId, method: 'Shelly.GetStatus', params:{id:0}}, function(result, error_code, error_message, slaves){
         
         if(error_code) {
-          log({error_code, error_message});
+          log({error_code: error_code, error_message: error_message});
         } else {
           let newValvePosition = result['trv:0'].pos;
-          syncSlaves(slaves, newValvePosition);
+          syncSlavesValvePosition(slaves, newValvePosition);
         }
 
       }, config.slaves);
